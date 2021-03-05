aeb48efa63f6af5c71d4ca650fe68ffc5725480f
Kien Do <doluongkien@gmail.com>
2021-03-05 10:43:49 +0700

    import

--- designGUI.py
+++ designGUI.py
@@ -3,13 +3,12 @@ import sys
 from PyQt5 import QtCore
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QFileDialog, QLabel, QMainWindow, QWidget, QPushButton, QDesktopWidget
+from PyQt5.QtWidgets import QApplication, QFileDialog, QLabel, QWidget, QPushButton, QDesktopWidget
 
-class MyWindow(QMainWindow):
+class MyWindow(QWidget):
 
     def __init__(self):
-        super(MyWindow, self).__init__()
-        self.centralWidget = QWidget(self)
+        super().__init__()
         self.setWindowTitle('GUI')
         self.resize(440, 680)
         self.initUI()
@@ -21,9 +20,10 @@ class MyWindow(QMainWindow):
         self.impButton.move(40, 440)
 
         self.label = QLabel(self)
-        self.pixmap = QPixmap()
-        self.impButton.clicked.connect(self.clickImp)
+        self.setLabel('anh.jpg')
 
+        self.impButton.clicked.connect(self.clickImp)
+        
         self.expButton = QPushButton('Export', self)
         self.expButton.resize(160, 40)
         self.expButton.move(240, 440)
@@ -43,7 +43,7 @@ class MyWindow(QMainWindow):
         self.undoButton = QPushButton('Undo', self)
         self.undoButton.resize(160, 40)
         self.undoButton.move(240, 600)
-
+        
         self.center()
         
 
@@ -54,18 +54,18 @@ class MyWindow(QMainWindow):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
+    def setLabel(self, imgPath):
+        pm = QPixmap(imgPath)
+        self.label.setPixmap(pm.scaled(360, 360, QtCore.Qt.KeepAspectRatio))
+        self.label.move(40, 40)
 
     def clickImp(self):
         options = QFileDialog.Options()
         # options |= QFileDialog.DontUseNativeDialog
-        fName, _ = QFileDialog.getOpenFileName(self, "Open a image", "", "All File(*);;Image files (*.jpg *.png)", options=options)
-        if fName:
-            self.pixmap = QPixmap(fName)
-            self.pixmap = self.pixmap.scaled(360, 360, QtCore.Qt.KeepAspectRatio)
-            self.label.setPixmap(self.pixmap)
-            self.label.move(40, 40)
-
-            # print(self.pixmap.size())
+        fName = QFileDialog.getOpenFileName(self, "Open a image","", "All File(*);;Image files (*.jpg *.png)", options=options)
+        imgPath = fName[0]
+        if imgPath:
+            self.setLabel(imgPath)
 
 
 def main():
